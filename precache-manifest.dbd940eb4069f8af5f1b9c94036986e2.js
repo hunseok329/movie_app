f24af35a61c32853240afb77d9fadf14dea49515
hunseak <kkhs5064@naver.com>
2020-04-11 21:26:34 +0900

    Updates

---
+++ precache-manifest.dbd940eb4069f8af5f1b9c94036986e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dddfec779547e0f74791d8a31e88217",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1bdb26c443ef7a85f1ac",
+    "url": "/movie_app/static/css/main.9df05f8c.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdb26c443ef7a85f1ac",
+    "url": "/movie_app/static/js/main.866d074c.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
